10e475dd32e7b9c90b39aa0acd1b1a3d9adc6415
milicarabelos <mcarabelos@seenka.com>
2026-02-09 09:34:47 -0300

    fix(admin_panel):erease extra lines

--- admin_panel/app/config.py
+++ admin_panel/app/config.py
@@ -5,12 +5,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     MONGO_INITDB_DATABASE: str
 
-    JWT_PUBLIC_KEY: str
-    JWT_PRIVATE_KEY: str
-    REFRESH_TOKEN_EXPIRES_IN: int
-    ACCESS_TOKEN_EXPIRES_IN: int
-    JWT_ALGORITHM: str
-
     CLIENT_ORIGIN: str
 
     class Config:
